83f238ff3b184e51f126e52bb8edf4ff90f42c7f
lawrencemboya <lawrencemboya@gmail.com>
2019-06-10 14:04:58 -0400

    make review class and getters

---
+++ src/main/java/wcci/acquisitionsinc/Review.java
@@ -0,0 +1,25 @@
+package wcci.acquisitionsinc;
+
+public class Review {
+
+	public int getID() {
+		return 0;
+	}
+
+	public String getTitle(){ 
+		return "";
+	}
+
+	public String getImageUrl() {
+		return "";
+	}
+
+	public String getReviewCategory() {
+		return "";
+	}
+
+	public String getContent() {
+		return "";
+	}
+
+}
