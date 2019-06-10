1507257b4477bbf7a72e569ebaccf5b4faf175a0
matthew-laine <matthew.i.laine@gmail.com>
2019-06-10 13:20:18 -0400

    first commit :

---
+++ src/main/java/wcci/acquisitionsinc/AcquisitionsIncApplication.java
@@ -0,0 +1,13 @@
+package wcci.acquisitionsinc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcquisitionsIncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcquisitionsIncApplication.class, args);
+	}
+
+}
---
+++ src/test/java/wcci/acquisitionsinc/AcquisitionsIncApplicationTests.java
@@ -0,0 +1,16 @@
+package wcci.acquisitionsinc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AcquisitionsIncApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
