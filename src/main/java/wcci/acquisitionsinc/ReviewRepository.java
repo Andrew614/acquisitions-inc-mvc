39d9e3e278fe8aea5db97f1a283ac231bf01c03c
Nick-Hergatt <nickmachergatt@gmail.com>
2019-06-11 10:25:52 -0400

    Test and Method for adding review to repository

--- src/main/java/wcci/acquisitionsinc/ReviewRepository.java
+++ src/main/java/wcci/acquisitionsinc/ReviewRepository.java
@@ -1,9 +1,19 @@
 package wcci.acquisitionsinc;
 
-import java.util.ArrayList;
+import java.util.HashMap;
 
 public class ReviewRepository {
+	HashMap<Integer, Review> reviews = new HashMap<Integer,Review>();
 
+	public void addReview(Review review) {
+		reviews.put(review.getID(), review);
+	}
+
+	public Review getReview(int id) {
+		return reviews.get(id);
+	}
+	
+	
 	
 
 }
