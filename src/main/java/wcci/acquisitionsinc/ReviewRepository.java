0bf7b8a24a9df648e6e7c24d57bc8624bc836b53
matthew-laine <matthew.i.laine@gmail.com>
2019-06-11 15:41:49 -0400

    remove imports and whitespace

--- src/main/java/wcci/acquisitionsinc/ReviewRepository.java
+++ src/main/java/wcci/acquisitionsinc/ReviewRepository.java
@@ -3,7 +3,7 @@ package wcci.acquisitionsinc;
 import java.util.HashMap;
 
 public class ReviewRepository {
-	HashMap<Integer, Review> reviews = new HashMap<Integer,Review>();
+	HashMap<Integer, Review> reviews = new HashMap<Integer, Review>();
 
 	public void addReview(Review review) {
 		reviews.put(review.getID(), review);
@@ -12,8 +12,5 @@ public class ReviewRepository {
 	public Review getReview(int id) {
 		return reviews.get(id);
 	}
-	
-	
-	
 
 }
