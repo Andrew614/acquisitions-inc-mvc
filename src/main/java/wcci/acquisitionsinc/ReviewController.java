adaab8cddbb0c228ae424f6783944caf00ba2fcb
Andrew614 <azhang213@gmail.com>
2019-06-13 09:45:56 -0400

    update annottions

--- src/main/java/wcci/acquisitionsinc/ReviewController.java
+++ src/main/java/wcci/acquisitionsinc/ReviewController.java
@@ -1,15 +1,20 @@
 package wcci.acquisitionsinc;
 
+import javax.annotation.Resource;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/reviews")
 public class ReviewController {
-	ReviewRepository reviewRepository = new ReviewRepository();
+	
+	@Resource
+	ReviewRepository reviewRepository;
 
-	@RequestMapping("/reviews/")
+	@RequestMapping("/")
 	public String getReviews(Model model) {
 		reviewRepository.populateReviews();
 		model.addAttribute("reviews", reviewRepository.getAllReviews());
@@ -17,7 +22,7 @@ public class ReviewController {
 
 	}
 
-	@RequestMapping("/reviews/{id}")
+	@RequestMapping("/{id}")
 	public String getReview(@PathVariable("id") long id, Model model) {
 		model.addAttribute("review", reviewRepository.getReview(id));
 		return "review";
--- src/main/java/wcci/acquisitionsinc/ReviewRepository.java
+++ src/main/java/wcci/acquisitionsinc/ReviewRepository.java
@@ -3,9 +3,17 @@ package wcci.acquisitionsinc;
 import java.util.Collection;
 import java.util.HashMap;
 
+import org.springframework.stereotype.Repository;
+
+
+@Repository
 public class ReviewRepository {
 	private static HashMap<Long, Review> reviews = new HashMap<Long, Review>();
 
+	public ReviewRepository() {
+		populateReviews();
+	}
+	
 	public void addReview(Review review) {
 		reviews.put(review.getId(), review);
 	}
@@ -20,6 +28,7 @@ public class ReviewRepository {
 
 	public void populateReviews() {
 		addReview(new Review(1, "title", "image", "category", "content"));
+		addReview(new Review(2, "title2", "image2", "category2", "content2"));
 		
 	}
 
