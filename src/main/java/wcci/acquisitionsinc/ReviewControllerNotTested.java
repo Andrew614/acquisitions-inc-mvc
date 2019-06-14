df1bfcde4648c68f3ddc3902d8a6df62269b9019
Nick-Hergatt <nickmachergatt@gmail.com>
2019-06-14 15:34:16 -0400

    Web layer testing, Mock Injection in controller test

--- src/main/java/wcci/acquisitionsinc/ReviewControllerNotTested.java
+++ src/main/java/wcci/acquisitionsinc/ReviewControllerNotTested.java
@@ -1,31 +1,31 @@
-package wcci.acquisitionsinc;
-
-import javax.annotation.Resource;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-@Controller
-
-@RequestMapping({"/reviews","/reviews/"})
-public class ReviewController {
-
-	
-	@Resource
-	ReviewRepository reviewRepository;
-
-	@RequestMapping({"/", ""})
-	public String getReviews(Model model) {
-		model.addAttribute("reviews", reviewRepository.getAllReviews());
-		return "reviews";
-
-	}
-
-	@RequestMapping({"/{id}", "/{id}/"})
-	public String getReview(@PathVariable("id") long id, Model model) {
-		model.addAttribute("review", reviewRepository.getReview(id));
-		return "review";
-	}
-}
+//package wcci.acquisitionsinc;
+//
+//import javax.annotation.Resource;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.PathVariable;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//
+//@Controller
+//
+//@RequestMapping({"/reviews","/reviews/"})
+//public class ReviewController {
+//
+//	
+//	@Resource
+//	ReviewRepository reviewRepository;
+//
+//	@RequestMapping({"/", ""})
+//	public String getReviews(Model model) {
+//		model.addAttribute("reviews", reviewRepository.getAllReviews());
+//		return "reviews";
+//
+//	}
+//
+//	@RequestMapping({"/{id}", "/{id}/"})
+//	public String getReview(@PathVariable("id") long id, Model model) {
+//		model.addAttribute("review", reviewRepository.getReview(id));
+//		return "review";
+//	}
+//}
