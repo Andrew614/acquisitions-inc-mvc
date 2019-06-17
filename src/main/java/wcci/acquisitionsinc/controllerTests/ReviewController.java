ba2e1560100c1eb1f52fe85c4f24452449719f1f
Nick-Hergatt <nickmachergatt@gmail.com>
2019-06-17 11:08:33 -0400

    Refactor controller and test for mockito injection

--- src/main/java/wcci/acquisitionsinc/controllerTests/ReviewController.java
+++ src/main/java/wcci/acquisitionsinc/controllerTests/ReviewController.java
@@ -1,8 +1,11 @@
 package wcci.acquisitionsinc.controllerTests;
 
-import java.util.Collection;
+import javax.annotation.Resource;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import wcci.acquisitionsinc.Review;
 import wcci.acquisitionsinc.ReviewRepository;
@@ -10,15 +13,19 @@ import wcci.acquisitionsinc.ReviewRepository;
 @Controller
 public class ReviewController {
 
+	@Resource
+	ReviewRepository reviewRepo;
 	
-	ReviewRepository reviewRepo = new ReviewRepository();
-	
-	public Collection<Review> getReviews() {
-		return reviewRepo.getAllReviews();
+	@RequestMapping({"/reviews/","/reviews"})
+	public String getReviews(Model model) {
+		model.addAttribute("reviews", reviewRepo.getAllReviews());
+		return "reviews";
 	}
 	
-	public Review getReview(long id) {
-		return reviewRepo.getReview(id);
+	@RequestMapping({"/reviews/1","/reviews/1/"})
+	public String getReview(long id, Model model) {
+		model.addAttribute("review", reviewRepo.getReview(id));
+		return "review";
 	}
 
 }
--- src/test/java/wcci/acquisitionsinc/integrationsTests/WebLayerTest.java
+++ src/test/java/wcci/acquisitionsinc/integrationsTests/WebLayerTest.java
@@ -9,9 +9,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -27,5 +25,12 @@ public class WebLayerTest {
 	public void shouldReturnReviewsPage() throws Exception {
 		this.mockMvc.perform(get("/reviews")).andDo(print()).andExpect(status().isOk());
 	}
+	
+	@Test
+	public void shouldReturnReviewPage() throws Exception {
+		this.mockMvc.perform(get("/reviews/1")).andDo(print()).andExpect(status().isOk());
+	}
+	
+
 	
 }
