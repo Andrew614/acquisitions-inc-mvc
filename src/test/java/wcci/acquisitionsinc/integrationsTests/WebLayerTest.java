5546bd15be183e45462ea341508e9da871c46b5e
matthew-laine <matthew.i.laine@gmail.com>
2019-06-17 11:42:38 -0400

    weblayertest passes for /review and /reviews

--- src/test/java/wcci/acquisitionsinc/integrationsTests/WebLayerTest.java
+++ src/test/java/wcci/acquisitionsinc/integrationsTests/WebLayerTest.java
@@ -10,8 +10,11 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+
+import wcci.acquisitionsinc.ReviewRepository;
 
 
 @RunWith(SpringRunner.class)
@@ -21,16 +24,22 @@ public class WebLayerTest {
 	@Autowired
 	private MockMvc mockMvc;
 	
+	@MockBean
+	private ReviewRepository reviewRepo;
+	
 	@Test
 	public void shouldReturnReviewsPage() throws Exception {
 		this.mockMvc.perform(get("/reviews")).andDo(print()).andExpect(status().isOk());
 	}
 	
-//	@Test
-//	public void shouldReturnReviewPage() throws Exception {
-//		this.mockMvc.perform(get("/reviews/1")).andDo(print()).andExpect(status().isOk());
-//	}
+	@Test
+	public void shouldReturnReview1Page() throws Exception {
+		this.mockMvc.perform(get("/reviews/1")).andDo(print()).andExpect(status().isOk());
+	}
 	
-
+	@Test
+	public void shouldReturnReview2Page() throws Exception {
+		this.mockMvc.perform(get("/reviews/2")).andDo(print()).andExpect(status().isOk());
+	}
 	
 }
